909c5e80c12c6e45faee87dcf66d89b8f857bbce
Evilink-Demon <verashchakovivan@gmail.com>
2026-09-16 15:55:42 +0300

    Добавлена модель WasteItem(models.py)

--- backend/waste/models.py
+++ backend/waste/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class WasteItem(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+    recyclable = models.BooleanField()
+    preparation = models.TextField()
+
+def __str__(self):
+    return self.name
